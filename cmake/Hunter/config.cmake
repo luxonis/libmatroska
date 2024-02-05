3706d85d248f4f583f035995d9da595f176e6193
asahtik <asahtik@gmail.com>
2024-02-05 15:38:47 +0100

    Use latest EBML

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,8 +1,8 @@
 hunter_config(
     EBML
-    VERSION "1.4.5"
-    URL "https://github.com/Matroska-Org/libebml/archive/refs/tags/release-1.4.5.tar.gz"
-    SHA1 "9d1e823d7be5d0a0699c4cca0354082e277b7d91"
+    VERSION "2.0.0"
+    URL "https://github.com/Matroska-Org/libebml/archive/59bca6e93a9e6df7cb0ac527a2d2a31b0037afa8.tar.gz"
+    SHA1 "26130a30ed9ebfd47dda395371de26be02615f88"
     CMAKE_ARGS
         # Build shared libs by default to not cause licensing issues
         BUILD_SHARED_LIBS=ON
