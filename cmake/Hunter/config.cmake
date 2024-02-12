3c71c52e47244b7529151c6d5007be06bfa3f164
asahtik <asahtik@gmail.com>
2024-02-12 14:11:22 +0100

    Bump ebml

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,8 +1,8 @@
 hunter_config(
     EBML
-    VERSION "1.4.3"
-    URL "https://github.com/Matroska-Org/libebml/archive/refs/tags/release-1.4.3.tar.gz"
-    SHA1 "3d45f72fe065bc39a7ea90bdcf6d598c645b786b"
+    VERSION "1.4.5"
+    URL "https://github.com/Matroska-Org/libebml/archive/fc7a329e6e3ae5576ec154312499a0e53be4e0e4.tar.gz"
+    SHA1 "cfa4f2a7f5046f192f7770e6109b3b09e125af34"
     CMAKE_ARGS
         # Build shared libs by default to not cause licensing issues
         BUILD_SHARED_LIBS=ON
